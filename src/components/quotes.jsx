4629f8d485ff8a11e7a1ce4ca8ae0268d2447e2f
Sean R Reid <sean@torchcodelab.com>
2020-03-24 15:24:07 -0400

    Added a timeout around the setState() method inside the didMount()

--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -1,16 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Quote extends Component {
-    state = {
-        quote: "Chuck Norris does rad stuff!"
-    }
+  state = {
+    quote: 'Chuck Norris does rad stuff!'
+  };
 
-    render() {
-        const { quote } = this.state;
-        return (
-            <p>{quote}</p>
-        );
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        quote: 'Chuck waits for no one!!'
+      });
+    }, 2000);
+  }
+
+  render() {
+    const { quote } = this.state;
+    return <p>{quote}</p>;
+  }
 }
 
 export default Quote;
