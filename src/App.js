a3271345d80784509ceacad743bb344db5376354
Sean R Reid <sean@torchcodelab.com>
2020-03-24 12:35:25 -0400

    Added a quote component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import Quote from './components/quotes';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">CHUCK SAYS!!</header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">CHUCK SAYS!!</header>
+      <Quote />
+    </div>
+  );
 }
 
 export default App;
